1d87eec4f5a9eae435d5f6e1358eab5b957630ef
Chiichriis <chichris28@gmail.com>
2022-09-09 04:49:17 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - detemined if random input is positive, nagative or zero.
+ *
+ * Return: 0 on sucess.
+ * 
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else if(n < 0)
+	{
+		printf("%d is %s\n", n, "nagative");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
+
+}
