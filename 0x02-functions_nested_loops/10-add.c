f2c89dac1d91945a355cc102ba2d6ac361aa23de
Chiichriis <chichris28@gmail.com>
2022-09-13 08:30:09 -0700

    task 10 additon func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @x: int type num
+ * @y: int tupe num
+ * Return: the result
+ */
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
