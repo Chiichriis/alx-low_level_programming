b117255f288ffea47e3216510072f2c2c5d259bf
Chiichriis <chichris28@gmail.com>
2022-09-13 06:01:19 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar then new line
+ * 
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char temp[] = "_putchar";
+	int i = 0;
+
+	while (temp[i] != '\0')
+	{
+		putchar(temp[i]);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
